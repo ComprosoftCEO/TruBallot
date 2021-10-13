9599ad40cc794743bee1375be8814f58aa3cf137
Bryan McClain <bamcclain10@gmail.com>
2021-10-13 11:49:37 -0400

    Get cancelation shares now accepts a vector of user IDs

--- collector/src/handlers/election/get_cancelation_shares.rs
+++ collector/src/handlers/election/get_cancelation_shares.rs
@@ -14,7 +14,8 @@ use crate::views::election::CancelationShares;
 #[derive(Debug, Deserialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct CancelationSharesData {
-  user_id: Uuid,
+  #[validate(length(min = 1))]
+  user_ids: Vec<Uuid>,
 }
 
 pub async fn get_cancelation_shares(
@@ -27,31 +28,43 @@ pub async fn get_cancelation_shares(
   data.validate()?;
 
   let (election_id, question_id) = path.into_inner();
-  let CancelationSharesData { user_id } = data.into_inner();
+  let CancelationSharesData { user_ids } = data.into_inner();
 
-  // Make sure the election and registration exist
+  // Make sure the election and all registrations exist
   let election = Election::find_resource(&election_id, &conn)?;
-  let registration = election
-    .get_registration(&question_id, &user_id, &conn)?
-    .ok_or_else(|| ServiceError::UserNotRegistered {
-      user_id,
-      election_id,
-      question_id: Some(question_id),
-    })?;
+  let registrations = user_ids
+    .into_iter()
+    .map(|user_id| {
+      Ok(
+        election
+          .get_registration(&question_id, &user_id, &conn)?
+          .ok_or_else(|| ServiceError::UserNotRegistered {
+            user_id,
+            election_id,
+            question_id: Some(question_id),
+          })?,
+      )
+    })
+    .collect::<Result<Vec<_>, ServiceError>>()?;
 
-  // Compute the shares
+  // Compute the sum of the shares for all users
   let modulus = election.prime.to_bigint() - 1;
-
-  let forward_cancelation_shares = BigInt::mod_sub(
-    &registration.forward_ballot_shares.to_bigint(),
-    &registration.forward_verification_shares.to_bigint(),
-    &modulus,
-  );
-
-  let reverse_cancelation_shares = BigInt::mod_sub(
-    &registration.reverse_ballot_shares.to_bigint(),
-    &registration.reverse_verification_shares.to_bigint(),
-    &modulus,
+  let (forward_cancelation_shares, reverse_cancelation_shares) = registrations.into_iter().fold(
+    (BigInt::from(0), BigInt::from(0)),
+    |(forward, reverse), registration| {
+      (
+        BigInt::mod_add(
+          &forward,
+          &(registration.forward_ballot_shares - registration.forward_verification_shares).to_bigint(),
+          &modulus,
+        ),
+        BigInt::mod_add(
+          &reverse,
+          &(registration.reverse_ballot_shares - registration.reverse_verification_shares).to_bigint(),
+          &modulus,
+        ),
+      )
+    },
   );
 
   Ok(HttpResponse::Ok().json(CancelationShares {
