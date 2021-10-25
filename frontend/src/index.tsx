af79d97877a51685d62c3a76eed7605d0f91400a
Bryan McClain <bamcclain10@gmail.com>
2021-10-24 22:44:10 -0400

    Added semantic UI React and a custom theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
 
+// Import our Semantic UI theme
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'semantic-ui-less/semantic.less';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
