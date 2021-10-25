8d8e9b4b2e2235fb2d6698a7500849f2949a3360
Bryan McClain <bamcclain10@gmail.com>
2021-10-24 22:02:28 -0400

    Bootstrapped a basic React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={`${process.env.PUBLIC_URL}/truballot-logo.svg`} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
