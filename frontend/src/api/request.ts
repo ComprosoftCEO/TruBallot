2309c4f7972ba8229ac352c1aadf4d972186a62c
Bryan McClain <bamcclain10@gmail.com>
2021-11-02 01:23:02 -0400

    Better error handling in refresh JWT function

--- frontend/src/api/request.ts
+++ frontend/src/api/request.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
-import { applyAuthTokenInterceptor, TokenRefreshRequest } from 'axios-jwt';
+import { applyAuthTokenInterceptor, TokenRefreshRequest, clearAuthTokens } from 'axios-jwt';
 import { ClientToken, LoginResult } from 'models/auth';
 import { API_BASE_URL } from 'env';
 import { mergeNestedState } from 'redux/helpers';
@@ -11,29 +11,42 @@ import { resolveOptionUnwrapped } from 'api';
  */
 export const axiosApi = axios.create({ baseURL: API_BASE_URL });
 
+const mergeState = mergeNestedState('globals');
+
 // Define the refresh function
 const requestRefresh: TokenRefreshRequest = async (refreshToken) => {
-  const response = await axios
-    .post<LoginResult>(`${API_BASE_URL}/auth/refresh`, undefined, {
-      headers: {
-        Authorization: `Bearer ${refreshToken}`,
-      },
-    })
-    .then(...resolveOptionUnwrapped);
+  try {
+    const response = await axios
+      .post<LoginResult>(`${API_BASE_URL}/auth/refresh`, undefined, {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      })
+      .then(...resolveOptionUnwrapped);
 
-  // Update the store with the new JWT token
-  const clientToken: ClientToken = jwt.decode(response.data.clientToken) as ClientToken;
-  mergeNestedState('globals', {
-    isLoggedIn: true,
-    name: clientToken.name,
-    email: clientToken.email,
-    permissions: new Set(clientToken.permissions),
-  });
+    // Update the store with the new JWT token
+    const clientToken: ClientToken = jwt.decode(response.data.clientToken) as ClientToken;
+    mergeState({
+      isLoggedIn: true,
+      name: clientToken.name,
+      email: clientToken.email,
+      permissions: new Set(clientToken.permissions),
+    });
 
-  return {
-    accessToken: response.data.clientToken,
-    refreshToken: response.data.refreshToken,
-  };
+    return {
+      accessToken: response.data.clientToken,
+      refreshToken: response.data.refreshToken,
+    };
+  } catch (error) {
+    // Log out the user if some sort of refresh error occurs
+    //   This prevents a nasty infinite loop if the API server ever goes down
+    clearAuthTokens();
+    mergeState({
+      isLoggedIn: false,
+    });
+
+    throw error;
+  }
 };
 
 // Add interceptor to the axios instance
